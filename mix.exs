56a08e4a226f01857a747420183f7e29d349d6ad
Mudassar Ali <Mudassaralics007@gmail.com>
2021-06-04 22:18:43 +0500

    update deps

--- mix.exs
+++ mix.exs
@@ -33,15 +33,19 @@ defmodule Vocial.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.4"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
+      {:phoenix, "~> 1.5.9"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.10"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_dashboard, "~> 0.4"},
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:plug_cowboy, "~> 1.0"}
+      {:jason, "~> 1.0"},
+      {:poison, "~> 4.0.1"},
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 
